03d3ccdc87a7bb45c8d3f2fd0d665845cd6e01df
Damil_Berdyev <berdyev.damil1@mail.ru>
2023-03-15 09:02:43 +0600

    feat: vite deploy in config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: '/vite-deploy-demo/',
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },

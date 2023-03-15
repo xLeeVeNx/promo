d571b2ec7f953d6a899657a36e204231e8988264
Damil_Berdyev <berdyev.damil1@mail.ru>
2023-03-15 09:05:57 +0600

    fix: base name in config for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: '/vite-deploy-demo/',
+  base: '/promo/',
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
